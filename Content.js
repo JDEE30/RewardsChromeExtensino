450ea8f17a1353bac9b9ad7150986ff200e14712
Jeremy Dickey <47309416+JDEE30@users.noreply.github.com>
2021-07-29 16:54:24 -0500

    updating extension

--- Content.js
+++ Content.js
@@ -1,1 +1,95 @@
-alert("helloo");
+const generateSTYLES = () => {
+    return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
+
+    .myModal {
+      text-align: center;
+      display: block;
+      position: relative;
+      width: 80%;
+      margin: 100px auto;
+      background
+      background: #FFFFF;
+      color: #000000;
+      font-family: "Open Sans", sans-serif;
+      max-height: 700px;
+      overflow: hidden;
+    }
+   
+    .text {
+      font-size: 70px;
+      text-align: center;
+      position: relative;
+      display: inline-block;
+      margin: 19px 0px 0px 0px;
+      /* top: 256.301px; */
+      z-index: 3;
+      width: 100%;
+      line-height: 1.2em;
+      display: inline-block;
+    
+    
+    }
+     </style>`;
+  };
+  
+  const generateHTML = (pageName) => {
+    return `
+     
+     <div id="clouds">
+        <div class="cloud x1"></div>
+        <div class="cloud x1_5"></div>
+        <div class="cloud x2"></div>
+        <div class="cloud x3"></div>
+        <div class="cloud x4"></div>
+        <div class="cloud x5"></div>
+    </div>
+    <div class='myModal'>
+        <div class='_404'>CC REWARDS</div>
+        <hr>
+        <div class='_1'>
+        Thanks for using  our extension. <br>
+        
+        
+        
+        For your security all data is saved  <br>
+        
+        locally on your browser.
+        <br>
+        
+        
+        this means We will need to calibrate <br>
+        
+         the extension</div>
+        <div class='_2'>After calibration, we will bring back ${pageName}</div>
+    </div>
+     `;
+  };
+  
+  switch (window.location.hostname) {
+    case "www.youtube.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("YOUTUBE");
+      break;
+    case "www.facebook.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("FACEBOOK");
+      break;
+    case "www.netflix.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("NETFLIX");
+      break;
+    case "www.roblox.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("ROBLOX");
+      break;
+    case "discord.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("DISCORD");
+      break;
+    case "www.spotify.com":
+      document.head.innerHTML = generateSTYLES();
+      document.body.innerHTML = generateHTML("SPOTIFY");
+      break;
+  }
+
+   
