3fdd88c8e7ae69f2e98879282f1fe2b671083302
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-19 23:09:10 +0530

    injected foreground script on webpages succefully

--- background.js
+++ background.js
@@ -9,4 +9,15 @@ chrome.storage.local.get(['name', 'age'], data => {
 
 });
 
-console.log("background.js working");
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        chrome.scripting.executeScript({
+            target: {tabId: tabId},
+            files: ["./foreground.js"]
+        })
+            .then(() => {
+                console.log("Injected foreground script")
+            })
+            .catch(err => console.log(err));
+    }
+});
