a33fb3d636ae8897e74f79c6893b2252be0b68d9
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-19 23:13:36 +0530

    [test] rotate googles logo on google homepage using foreground script

--- foreground.js
+++ foreground.js
@@ -1,1 +1,2 @@
-console.log("foreground.js working");
+document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transitionDuration = '3.0s';
+document.querySelector("body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img").style.transform = 'rotate(900deg)';
