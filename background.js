865b1dd048c91927a6dfa32dad510b3b1631f7d5
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-21 00:05:28 +0530

    added and linked foreground styles

--- background.js
+++ background.js
@@ -10,13 +10,21 @@ chrome.storage.local.get(['name', 'age'], data => {
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if(changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        chrome.scripting.executeScript({
-            target: {tabId: tabId},
-            files: ["./foreground.js"]
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabId },
+            files: ["./foreground_styles.css"]
         })
             .then(() => {
-                console.log("Injected foreground script")
+                console.log("Injected foreground styles");
+
+                chrome.scripting.executeScript({
+                    target: { tabId: tabId },
+                    files: ["./foreground.js"]
+                })
+                    .then(() => {
+                        console.log("Injected foreground script");
+                    });
             })
             .catch(err => console.log(err));
     }
