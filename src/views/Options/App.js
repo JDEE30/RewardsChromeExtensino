bd585ed836d51112c9784b0c4367738292043a91
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-24 21:24:47 +0530

    added option page

--- src/views/Options/App.js
+++ src/views/Options/App.js
@@ -1,25 +1,67 @@
-import React from 'react'
-import './App.css'
+import { Card, CardHeader } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { getDataFromStorage } from "../../common/storageUtil";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Options page</p>
-        <p>
-          Edit <code>src/views/Options/App.js</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [storageData, setStorageData] = useState({ card_data: {} });
+	useEffect(() => {
+		getDataFromStorage().then((response) => {
+			setStorageData(response);
+		});
+	}, []);
+
+	const getIconUrlByVendor = (vendor) => {
+		let src = "creditCard.png";
+		switch (vendor) {
+			case "visa":
+				src = "visa.png";
+				break;
+			case "jcb":
+				src = "jcb.png";
+				break;
+			case "mastercard":
+				src = "mastercard.png";
+				break;
+			case "amex":
+				src = "amex.png";
+				break;
+			case "discover":
+				src = "discover.png";
+				break;
+			case "maestro":
+				src = "maestro.png";
+				break;
+			default:
+				break;
+		}
+		return src;
+	};
+
+	return (
+		<div className="card-list">
+			{Object.keys(storageData.card_data).length > 0 && (
+				<React.Fragment>
+					{Object.values(storageData.card_data).map((card) => (
+						<Card key={card.card_number} style={{ marginBottom: 5 }}>
+							<CardHeader
+								avatar={
+									<img
+										src={getIconUrlByVendor(card.card_vendor)}
+										width="50px"
+										height="50px"
+									/>
+								}
+								title={`Number: ${card.card_number.replace(/\d(?=\d{4})/g, "*")}`}
+								subheader={`CVV: ${card.card_cvv}`}
+							></CardHeader>
+						</Card>
+					))}
+				</React.Fragment>
+			)}
+			{Object.keys(storageData.card_data).length === 0 && <h2>No Data Found</h2>}
+		</div>
+	);
 }
 
-export default App
+export default App;
