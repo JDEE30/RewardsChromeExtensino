073ebf3e7f62ee3ca29ea54d49b06a3a233ae9de
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-28 20:52:36 +0530

    initial setup Best Card Page

--- src/views/Popup/components/BestCard.js
+++ src/views/Popup/components/BestCard.js
@@ -1,1 +1,16 @@
-//Best Card Page
+import React, { useState } from "react";
+
+const BestCard = (props) => {
+	const { categories, setCategories, setPageNo } =
+		props;
+
+
+		console.log('Best Card Page');
+	return (
+		<div className="bestCard">
+			<h2>The Best Card For *Category* is:</h2>
+		</div>
+	);
+};
+
+export default BestCard;
