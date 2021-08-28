c924083e93a2487f3119c008ea265e1c828baefb
deviouscyborg <vaibhavmay2@gmail.com>
2021-08-28 20:52:16 +0530

    initial setup of addCategory Page

--- src/views/Popup/components/AddCategory.js
+++ src/views/Popup/components/AddCategory.js
@@ -1,1 +1,16 @@
-//Add Category
+import React, { useState } from "react";
+
+const AddCategory = (props) => {
+	const { categories, setCategories, setPageNo } =
+		props;
+
+
+		console.log('Add Category Page');
+	return (
+		<div className="addCategory">
+			<h2>Add New Category</h2>
+		</div>
+	);
+};
+
+export default AddCategory;
